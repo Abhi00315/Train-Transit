66f83f7ab9e0d7e9acd749fe1cfd15b35c410da2
Abhi00315 <abhinav.saravanakumar@gmail.com>
2024-06-20 16:15:25 +0530

    Initial commit

--- lib/pages/login/sign_up.dart
+++ lib/pages/login/sign_up.dart
@@ -8,10 +8,10 @@ class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
   @override
-  _SignUpPageState createState() => _SignUpPageState();
+  SignUpPageState createState() => SignUpPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
+class SignUpPageState extends State<SignUpPage> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
