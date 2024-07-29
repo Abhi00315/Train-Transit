4294472cffa065985c26c9385ce1ad5793783c0e
Abhi00315 <abhinav.saravanakumar@gmail.com>
2024-07-29 23:03:17 +0530

    Initial commit

--- lib/pages/activity/booking_page.dart
+++ lib/pages/activity/booking_page.dart
@@ -83,7 +83,7 @@ class BookingPageState extends State<BookingPage> {
           );
         });
       } else {
-        // If the user is not authenticated, show an error message
+        // If the user is not authenticated, show an error messag
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('User not authenticated. Please log in.')),
         );
